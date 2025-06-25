69d534e09fd3305ff449b0b39d78852f201a5e22
https-helios <guan.s@oundleschool.org.uk>
2025-06-25 09:58:22 +0100

    second

--- install.php
+++ install.php
@@ -1,45 +1,71 @@
 <?php
 try{
     //creating user schema
-    $db->exec(statement: 'CREATE SCHEMA IF NOT EXISTS "SEN";');
+    $db->exec( 'CREATE SCHEMA IF NOT EXISTS "SEN";');
     echo"<br><br>Schema 'SEN' created successfully";
     $db->exec("");
 
     //creating UUID extention for random numbers
-    $db->exec(statement: 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp":');
+    $db->exec( 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp":');
     echo"<br> Extention for UUID has been created";
 
     //dropping Student table
-    $db-exec(statement: 'DROP TABLE IF EXISTS "SEN"."tblStudent" CASCADE;');
+    $db-exec( 'DROP TABLE IF EXISTS "SEN"."tblStudent" CASCADE;');
     echo"<br> Table 'tblStudent' has been dropped";
 
     //dropping Tutor table
-    $db-exec(statement: 'DROP TABLE IF EXISTS "SEN"."tblTutor" CASCADE;');
+    $db-exec( 'DROP TABLE IF EXISTS "SEN"."tblTutor" CASCADE;');
     echo"<br> Table 'tblTutor' has been dropped";
 
     //dropping Diagnosis table
-    $db-exec(statement: 'DROP TABLE IF EXISTS "SEN"."tblDiag" CASCADE;');
+    $db-exec( 'DROP TABLE IF EXISTS "SEN"."tblDiag" CASCADE;');
     echo"<br> Table 'tblDiag' has been dropped";
 
     //dropping Barrier table
-    $db-exec(statement: 'DROP TABLE IF EXISTS "SEN"."tblBarrier" CASCADE;');
+    $db-exec( 'DROP TABLE IF EXISTS "SEN"."tblBarrier" CASCADE;');
     echo"<br> Table 'tblBarrier' has been dropped";
 
     //dropping Strategies table
-    $db-exec(statement: 'DROP TABLE IF EXISTS "SEN"."tblStrat" CASCADE;');
+    $db-exec( 'DROP TABLE IF EXISTS "SEN"."tblStrat" CASCADE;');
     echo"<br> Table 'tblStrat' has been dropped";
 
     //creating Student Table
-    $db->exec(statement: '
+    $db->exec( '
     CREATE TABLE "SEN"."tblStudent" (
         StudentID uuid DEFAULT gen_random_uuid() PRIMARY KEY,
         Forename text NOT NULL,
         Surname text NOT NULL,
         YearGroup smallint,
         TutorID smallint,
-        House text NOT NULL,
-    )'
-);
+        House text NOT NULL
+    )');
+    echo'<br>Table "tblStudent" has been created successfully';
+
+    //creating Tutor Table
+    $db->exec( '
+    CREATE TABLE "SEN"."tblTutor" (
+        TutorID uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+        Forename text NOT NULL,
+        Surname text NOT NULL,
+        House text NOT NULL
+    )');
+    echo"<br> Table 'tblTutor' has been created successfully";
+
+    //creating Diagnosis table
+    $db->exec( '
+    CREATE TABLE "SEN"."tblDiag" (
+        DiagID uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+        Label text NOT NULL
+    )');
+    echo"<br> Table 'tblDiag' has been created successfully";
+
+    //creating Barrrier Table
+    $db->exec( '
+    CREATE TABLE "SEN"."tblBarrier" (
+        BarrierID uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+
+    )');
+    echo"<br>
 }
 
 ?>
