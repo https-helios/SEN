056674e355545e458a2ca07a09eb1825919797af
https-helios <guan.s@oundleschool.org.uk>
2025-06-26 11:56:56 +0100

    fixes

--- Student.php
+++ Student.php
@@ -11,19 +11,19 @@
             Surname:<input type = "text" name="Surname"><br>
             House:<input type ="text" name ="House"><br>
             YearGroup:<input type ="number" name="YearGroup"><br>
-            <select name = "StudentID">
+            <select name = "TutorID"></select>
             <input type="submit" value="Add Student">
         </form>
         <?php
             include_once("connection.php");
-            $stmt=$conn->prepare('SELECT * FROM "SEN"."tblStudent"');
+            $stmt=$db->prepare('SELECT * FROM "SEN"."tblStudent"');
             $stmt->execute();
             while($row=$stmt->fetch(PDO::FETCH_ASSOC))
                 {
                     echo($row["Forename"].", ".$row["Surname"].",".$row["House"].",".$row["YearGroup"]."<br>");
                 }
 
-            $_POST["submit"]
+            // $_POST["submit"]
 
             try{
                 $stmt = $db->prepare('SELECT "TutorID" FROM "SEN"."tblTutor" ORDER BY "TutorID"');
@@ -31,7 +31,8 @@
     
                 while($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
                     $TutorID = htmlspecialchars($row["TutorID"]);
-                    echo '<option value =\"TutorID\"></option>';
+                    echo '<option value="' . $TutorID . '">' . $Forename . '</option>';
+
                 }
             } catch (PDOException $e) {
                 echo "Database error: " . $e->getMessage();
