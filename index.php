ce70f03aa999eed22a5828058a8786b221e62ee6
https-helios <guan.s@oundleschool.org.uk>
2025-06-26 09:27:48 +0100

    test 1 for index

--- index.php
+++ index.php
@@ -33,8 +33,11 @@
                   FirstName
               </h1>
               <label>First Name</lable>
-              <select name = "FistNameSearch">
+              <select name = "search_category" id="search_category_id" class = "form-control">
                 <option value = "" selected = "selected"></option>
+                {% for row in tblStudent %}
+                <option value = '{{row.StudentID}}'> {{row.Forename}} </option>
+                {% endfor %}
               </select>
               <?php
               include_once("connection.php");
