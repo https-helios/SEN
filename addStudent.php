898e42d8db4a472d48ceda5d5e8fb83fdf5b75b4
https-helios <guan.s@oundleschool.org.uk>
2025-06-26 11:21:48 +0100

    fixing conn->db

--- addStudent.php
+++ addStudent.php
@@ -13,15 +13,16 @@ print_r($_POST);
 // }
 
 try{
-    $stmt=$conn->prepare('INSERT INTO "SEN"."tblStudent"(Forename, Surname, House, YearGroup)
-VALUES (:Forename, :Surname, :House, :YearGroup)');
-$stmt->bindParam(":Forename", $_POST["Forename"]);
-$stmt->bindParam(":Surname", $_POST["Surname"]);
-$stmt->bindParam(":House", $_POST["House"]);
-$stmt->bindParam(":YearGroup", $_POST["YearGroup"]);
 
-$stmt->execute();
-$conn=null;
+    $stmt=$db->prepare('INSERT INTO "SEN"."tblStudent"(Forename, Surname, House, YearGroup)
+    VALUES (:Forename, :Surname, :House, :YearGroup)');
+    $stmt->bindParam(":Forename", $_POST["Forename"]);
+    $stmt->bindParam(":Surname", $_POST["Surname"]);
+    $stmt->bindParam(":House", $_POST["House"]);
+    $stmt->bindParam(":YearGroup", $_POST["YearGroup"]);
+
+    $stmt->execute();
+    $conn=null;
 }
 catch (PDOException $e) {
     echo "<br><br><br><b>Error: " . $e->getMessage() . "</b>";
